7a4bdfc2a2fe8a2415a328b852a7cf7bd6ee2f09
Vickyvackydev <vickyvacky5@gmail.com>
2024-08-14 21:43:26 +0100

    feat: new update

---
+++ src/data/WorkData.js
@@ -0,0 +1,84 @@
+// work data
+
+export const Work = [
+  {
+    id: 1,
+    name: "School Management System",
+    description:
+      "It is build using React JS, 4 users dashboards with complex functionalities.",
+    tags: ["react", "redux", "api", "material ui"],
+
+    demo: "https://bhss-school-frontend-react.vercel.app/",
+    github: "https://github.com/Vickyvackydev",
+  },
+  {
+    id: 2,
+    name: "Fitness club landing page",
+    description:
+      "A Fitness landing page using Reactjs, and framer-motion, and mobile responsive.",
+    tags: ["react"],
+
+    demo: "https://fitclubmyapp.pages.dev/",
+    github: "https://github.com/Vickyvackydev/fitclub",
+  },
+  {
+    id: 3,
+    name: "Car renting platform",
+    description:
+      "A simpple car renting platform built in Nextjs, tailwind css, typescript, and headlessUI, integrated with RapidAPI.",
+    tags: ["Nextjs", "RapidApi", "typescript", "tailwindcss"],
+
+    demo: "https://car-rentals-ckfv.vercel.app/",
+    github: "https://github.com/Vickyvackydev/car_rentals",
+  },
+  {
+    id: 4,
+    name: "Simple Dashboard Ui",
+    description:
+      "A cool looking Dashboard ui built in Nextjs, tailwind css, works on both light mode and dark mode based on system display.",
+    tags: ["Nextjs", "Tailwindcss", "Typescript"],
+
+    demo: "https://dashboard-ytn5.vercel.app/",
+    github: "https://github.com/Vickyvackydev/dashboard",
+  },
+  {
+    id: 5,
+    name: "Task Management System",
+    description:
+      "A fullstack web app, Nextjs, typescript, Firebase, strong programming expertise, fully functional tasks system with authentication",
+    tags: ["Nextjs", "Typescript", "framer-motion", "firebase"],
+
+    demo: "https://task-manager-gamma-rouge.vercel.app/",
+    github: "https://github.com/Vickyvackydev/taskky/",
+  },
+  {
+    id: 6,
+    name: "Spottr Admin Dashboard",
+    description:
+      "An admin dashboard built in Reactjs, typescript, api integration and payment integration,  messaging plus notifications functionalities, still under production.",
+    tags: ["react", "typescript", "api"],
+
+    demo: "https://agreeable-meadow-0cace3103.5.azurestaticapps.net/",
+    github: "https://github.com/Vickyvackydev/",
+  },
+  {
+    id: 7,
+    name: "A Simple Task Manager",
+
+    description:
+      "A simple task manager project written in Reactjs, tailwind css, typescript,Firebase for the backend and a cron job for task scheduling and execution.",
+    tags: ["react", "typescript", "firebase"],
+    demo: "https://task-schedule-app.vercel.app/",
+    github: "https://github.com/Vickyvackydev/task-schedule-app",
+  },
+  {
+    id: 8,
+    name: "E-commerce Car Site",
+
+    description:
+      "An e-commerce site built in Nextjs, typescript with filtering and data passing functionalities.",
+    tags: ["react", "typescript", "framer-motion"],
+    demo: "https://cars-hub-liart.vercel.app/",
+    github: "https://github.com/Vickyvackydev/cars-hub/",
+  },
+];
